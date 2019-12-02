e160e3c9aacf3caded8adc2fada0c3488b7a2a12
Bethany Corcoran <fneb@users.noreply.github.com>
2019-12-02 20:14:08 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ class Header extends React.Component {
             <section id="header">
                 <div className="inner">
                     <span className="icon major fa-cloud"></span>
-                    <h1>We're <strong>Netfra</strong>, an IT<br />
-                    Professional Services company focussing on<br />
-                    networks and infrastructure.</h1>
-                    <p>Project management and outlining, Enterprise<br />
+                    <h1>We are <strong>Netfra</strong>.<br />
+                    We focus on IT Networks and Infrastructure<br />
+                    design, projects, management.</h1>
+                    <p>Project management and planning, Enterprise<br />
                     Architecture, ongoing maintenance. We do it all.</p>
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Discover</a></li>
