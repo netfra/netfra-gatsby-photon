d2e46ea7c58b2f556788464afec72d4e21a31032
Bethany Corcoran <fneb@users.noreply.github.com>
2019-12-02 20:10:55 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+#                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+#                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+#                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+#                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                     <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
